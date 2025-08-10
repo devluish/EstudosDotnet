4b21b3b32a7bad12f3f6a7fb076945476f1785b1
devluish <guibola36@gmail.com>
2025-08-09 21:41:26 -0300

    Commit

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,7 +1,19 @@
 ﻿// Screen Sound
 
+// Variáveis, parâmetros e campos privados -> Usar camelCase
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-Console.WriteLine(mensagemDeBoasVindas);
+
+// Métodos(incluindo void), propriedades e classes -> Usar PascalCase
+// Todas começam com letra maiúscula
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine("************************");
+    Console.WriteLine(mensagemDeBoasVindas);
+    Console.WriteLine("************************");
+}
+
+ExibirMensagemDeBoasVindas();
+
 
 
 
